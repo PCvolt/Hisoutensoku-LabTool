f2f354e5e1325f19c551b39b9944a09e5507ed6e
Alexey-Ul <alekesei.ul@gmail.com>
2019-10-31 03:46:34 +0700

    Created branch. Added .gitignore Configured release build. Moved source files. Fixed name order for girls. Added girl name instead "P1" and "P2" in output console.

---
+++ LabTool/src/stdafx.h
@@ -0,0 +1,34 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifndef stdafx_h__
+#define stdafx_h__
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+//Standard C++ headers
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+//Standard headers
+#include <array>
+#include <d3d9.h>
+#include <mmsystem.h>
+#include <shlwapi.h>
+#include <sstream>
+#include <string>
+#include <windows.h>
+
+
+// reference additional headers your program requires here
+
+#endif // stdafx_h__
---
+++ LabTool/src/targetver.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef targetver_h__
+#define targetver_h__
+// Including SDKDDKVer.h defines the highest available Windows platform.
+
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
+
+#endif // targetver_h__
